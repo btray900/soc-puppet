11e4de08d2fe1cab21516d9b6ebc3fee891177e2
root <punkinnovation@gmail.com>
2016-10-16 21:43:22 -0400

    Fix relay and mailx alerts

--- puppet/modules/postfix/manifests/init.pp
+++ puppet/modules/postfix/manifests/init.pp
@@ -1,10 +1,28 @@
 class postfix {
+
 	package { 'postfix':
-		ensure => 'installed',
+		ensure => present,
+		before => File['/etc/postfix/main.cf'],
 	}
 
 	service { 'postfix':
-		ensure => 'running',
+		ensure => running,
+		require => Package['postfix'],
+	}
+
+	file { '/etc/postfix/main.cf':
+		owner => root,
+		group => root,
+		mode  => 644,
+		audit => content,
+		content => template('postfix/main.cf.erb'),
+		require => Package['postfix'],
+		notify => Exec["Bounce PostFix"],
+	}
+
+	exec { 'Bounce PostFix':
+		user => root,
+		refreshonly => true,
+		command => '/bin/systemctl restart postfix',
 	}
 }
-
